456a0d87ed71e925376fabb0831adbb601a34947
ZXTgbh <147132087+ZXTgbh@users.noreply.github.com>
2023-11-02 22:34:16 +0300

    Number 7

---
+++ 7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void fillAndPrintArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 100;
+    }
+
+    cout << "Массив: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(0, "");
+	int arr1[5], arr2[10];
+
+    fillAndPrintArray(arr1, 5);
+    fillAndPrintArray(arr2, 10);
+
+	return 0;
+}
